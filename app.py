8a1f11388f88d82635cd4168eb54decae08cc23b
mostafa12mohamed <deshaana316@gmail.com>
2026-03-10 23:07:18 +0200

    Flask API Update

--- app.py
+++ app.py
@@ -31,8 +31,10 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e), 'status': 'failed'}), 400
 
-# if __name__ == '__main__':
-   #  app.run(debug=True, port=5000)
- 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+# if __name__ == '__main__':
+#   app.run(debug=True, port=5000)
+ 
